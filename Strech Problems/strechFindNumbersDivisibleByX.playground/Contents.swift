e50445082a10498ece91a21ae6cd8df938c14987
kpfister <kpfister@me.com>
2016-05-10 09:50:38 -0600

    Final commit
    I was close to getting it... kinda

--- Strech Problems/strechFindNumbersDivisibleByX.playground/Contents.swift
+++ Strech Problems/strechFindNumbersDivisibleByX.playground/Contents.swift
@@ -2,4 +2,42 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// make a method that prints ever x number between 0 and y
+
+
+// Make a method, or func, that takes in a paramiter for x and a paramiter y and find every number x is divisable by.
+
+/*
+ for n in 0...100 {
+    var x = 3
+    var y = 100
+    if
+    
+    
+    
+}
+ 
+ */
+
+let x = 3
+let y = 100
+
+for x in 1...y{
+    
+}
+// answer below
+func divByX(x: Int, y: Int) {
+    let intArray = Array(1...y)
+    for int in intArray{
+        if int % x == 0 {
+            print(int)
+        }
+        
+    }
+}
+
+
+divByX(8, y: 120)
+
+//This is what the method should look like.
+
